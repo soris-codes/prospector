734adaa2060f1d2ef301814b688dd55333bbcef5
soris-codes <soris.cox@gmail.com>
2019-06-20 21:25:19 +0100

    Setting up React as a frontend app within Django

---
+++ prospector/frontend/src/components/App.js
@@ -0,0 +1,14 @@
+import ReactDOM from 'react-dom'
+import React, { Component } from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is the App</h1>
+      </div>
+    )
+  }
+}
+ReactDOM.render(<App />, document.getElementById('app'))
+
--- prospector/prospector/settings.py
+++ prospector/prospector/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'prospects',
     'rest_framework',
+    'frontend',
 ]
 
 MIDDLEWARE = [
