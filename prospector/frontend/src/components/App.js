972d5316e24a99aeca9458867d0c77fa2c49c46f
soris-codes <soris.cox@gmail.com>
2019-06-20 22:17:31 +0100

    Starting work on layout - going to try Bulma instead of Bootstrap

--- prospector/frontend/src/components/App.js
+++ prospector/frontend/src/components/App.js
@@ -1,12 +1,12 @@
 import ReactDOM from 'react-dom'
 import React, { Component } from 'react'
 
+import Header from './layout/Header'
+
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>This is the App</h1>
-      </div>
+      <Header />
     )
   }
 }
---
+++ prospector/frontend/src/components/layout/Header.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div>
+        <nav class="navbar" role="navigation" aria-label="main navigation">
+          <div class="navbar-brand">
+            <a class="navbar-item" href="#">
+            <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: Free, open source, & modern CSS framework based on Flexbox" width="112" height="28" />
+            </a>
+
+            <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            </a>
+          </div>
+        </nav>
+      </div>
+    )
+  }
+}
+
